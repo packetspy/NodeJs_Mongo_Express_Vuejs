63a3f117ce5c0dbd5e56ba1a53e35fb33a4097d7
Anderson Tomazeto <packetspy@gmail.com>
2021-07-01 10:20:49 -0300

    Adjust Connection Method

--- api/database/connection.js
+++ api/database/connection.js
@@ -2,8 +2,7 @@ const connectionString = process.env.MONGO_URL || 'mongodb://root:p%40ck3tspy@12
 const connectionAuthSource = process.env.MONGO_AUTH_SOURCE || {authSource:'admin'}
 
 const monk = require('monk');
-const database = monk(connectionString, connectionAuthSource);
-
-//const database = require('monk')(connectionString, connectionAuthSource);
+//const database = monk(connectionString, connectionAuthSource);
+const database = require('monk')(connectionString, connectionAuthSource);
 
 module.exports = database;
